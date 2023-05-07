e84a0557f85319049f689b29e8ccfbf3f525df3b
曹珂俭 <1849201815@qq.com>
2023-05-07 19:47:32 +0800

    添加leftBar

--- src/share/homeLeft.tsx
+++ src/share/homeLeft.tsx
@@ -1,4 +1,5 @@
-import { defineComponent, PropType } from 'vue';
+import { Divider } from 'vant';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './homeLeft.module.scss';
 export const HomeLeft = defineComponent({
   props: {
@@ -7,6 +8,13 @@ export const HomeLeft = defineComponent({
     }
   },
   setup: (props, context) => {
+    const currentIndex = ref(-1)
+    const linkMap: Record<number, string> = {
+      0: '访问我的github吧',
+      1: '请联系我wx：YYik021',
+      2: '这是我学习的地方~',
+      3: '打开QQ吧！',
+    }
     return () => (
       <section class={s.left}>
         <div class={s.info}>
@@ -24,6 +32,33 @@ export const HomeLeft = defineComponent({
               <p style={"font-size:0.8em;"}>一个建立于 21 世纪的个人网站，存活于互联网的边缘</p>
             </div>
             <svg class={s.svg2}><use xlinkHref='#lmarks'></use></svg>
+          </div>
+          <div class={s.social}>
+            <div class={s.link}>
+              <a href="https://github.com/CaoKejian" target="_blank" onMouseover={() => (currentIndex.value = 0)}
+                onMouseleave={() => (currentIndex.value = -1)}
+              >
+                <svg class={s.svg}><use xlinkHref='#github'></use></svg>
+              </a>
+              <a href="weixin://" target="_blank" onMouseover={() => (currentIndex.value = 1)}
+                onMouseleave={() => (currentIndex.value = -1)}>
+                <svg class={s.svg}><use xlinkHref='#wechat'></use></svg>
+              </a>
+              <a href="https://space.bilibili.com/1930084978?spm_id_from=333.1007.0.0" target="_blank" onMouseover={() => (currentIndex.value = 2)}
+                onMouseleave={() => (currentIndex.value = -1)}>
+                <svg class={s.svg}><use xlinkHref='#bilibili'></use></svg>
+              </a>
+              <a href="https://im.qq.com/index/" target="_blank" onMouseover={() => (currentIndex.value = 3)}
+                onMouseleave={() => (currentIndex.value = -1)}>
+                <svg class={s.svg}><use xlinkHref='#qq'></use></svg>
+              </a>
+            </div>
+            {
+              currentIndex.value !== -1 ?
+                <div>{
+                  linkMap[currentIndex.value]
+                }</div> : <div></div>
+            }
           </div>
         </div>
 
--- src/share/homeRight.tsx
+++ src/share/homeRight.tsx
@@ -1,54 +1,57 @@
-import { defineComponent, PropType} from 'vue';
+import { defineComponent, PropType } from 'vue';
 import s from './homeRight.module.scss';
-export const HomeRight= defineComponent({
-  props:{
-    name:{
-      type:String as PropType<string>
+export const HomeRight = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>
     }
   },
-  setup:(props,context)=>{
-    return ()=>(
+  setup: (props, context) => {
+    const toWebPage = (url: string) => {
+      window.open(url, '_blank');
+    }
+    return () => (
       <section class={s.right}>
-      <div class={s.info}>
-        <div class={s.function}>
-          <div class={[s.fn, s.cards]}>
-            <span> -「 山竹记账 」</span>
-            <svg class={s.svg}><use xlinkHref='#mangosteen'></use></svg>
+        <div class={s.info}>
+          <div class={s.function}>
+            <div class={[s.fn, s.cards]} onClick={() => toWebPage('http://8.130.24.249:8080/#')}>
+              <span> -「 山竹记账 」</span>
+              <svg class={s.svg}><use xlinkHref='#mangosteen'></use></svg>
+            </div>
+            <div class={[s.fn, s.cards]} onClick={() => toWebPage('https://hhstu.caokejian.club/#')}>
+              <span> -「 唯你黄科 」</span>
+              <svg class={[s.svg, s.wnhk]}><use xlinkHref='#wnhk'></use></svg>
+            </div>
           </div>
-          <div class={[s.fn, s.cards]}>
-            <span> -「 唯你黄科 」</span>
-            <svg class={[s.svg, s.wnhk]}><use xlinkHref='#wnhk'></use></svg>
+          <div class={s.links}>
+            <div class={s.line}>
+              <svg class={s.svg}><use xlinkHref='#content'></use></svg>
+              <span>网站列表</span>
+            </div>
+            <div class={s.lis}>
+              <div class={s.el}>
+                <div class={[s.li, s.cards]}> <svg class={s.svg}><use xlinkHref='#resume'></use></svg>简历</div>
+              </div>
+              <div class={s.el}>
+                <div class={[s.li, s.cards]}>  <svg class={s.svg}><use xlinkHref='#blog'></use></svg>博客</div>
+              </div>
+              <div class={s.el}>
+                <div class={[s.li, s.cards]}>   <svg class={s.svg}><use xlinkHref='#item'></use></svg>项目</div>
+              </div>
+              <div class={s.el}>
+                <div class={[s.li, s.cards]}>  <svg class={s.svg}><use xlinkHref='#github'></use></svg>Github</div>
+              </div>
+              <div class={s.el}>
+                <div class={[s.li, s.cards]}> <svg class={s.svg}><use xlinkHref='#juejin'></use></svg>掘金</div>
+              </div>
+              <div class={s.el}>
+                <div class={[s.li, s.cards]}> <svg class={s.svg}><use xlinkHref='#blog'></use></svg>
+                  暂未开放</div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class={s.links}>
-          <div class={s.line}>
-            <svg class={s.svg}><use xlinkHref='#content'></use></svg>
-            <span>网站列表</span>
-          </div>
-          <div class={s.lis}>
-            <div class={s.el}>
-              <div class={[s.li, s.cards]}> <svg class={s.svg}><use xlinkHref='#resume'></use></svg>简历</div>
-            </div>
-            <div class={s.el}>
-              <div class={[s.li, s.cards]}>  <svg class={s.svg}><use xlinkHref='#blog'></use></svg>博客</div>
-            </div>
-            <div class={s.el}>
-              <div class={[s.li, s.cards]}>   <svg class={s.svg}><use xlinkHref='#item'></use></svg>项目</div>
-            </div>
-            <div class={s.el}>
-              <div class={[s.li, s.cards]}>  <svg class={s.svg}><use xlinkHref='#github'></use></svg>Github</div>
-            </div>
-            <div class={s.el}>
-              <div class={[s.li, s.cards]}> <svg class={s.svg}><use xlinkHref='#juejin'></use></svg>掘金</div>
-            </div>
-            <div class={s.el}>
-              <div class={[s.li, s.cards]}> <svg class={s.svg}><use xlinkHref='#blog'></use></svg>
-                暂未开放</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
     )
   }
 })
