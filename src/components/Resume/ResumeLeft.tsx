b2d2d4f816bd2d86b1ca6d8a41cb74398443cac5
曹珂俭 <1849201815@qq.com>
2023-05-09 16:17:45 +0800

    修改resumeLeft展开、隐藏

--- src/components/Resume/ResumeLeft.tsx
+++ src/components/Resume/ResumeLeft.tsx
@@ -1,4 +1,5 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
+import { Button } from '../../share/Button';
 import { Bar } from './Bar';
 import s from './ResumeLeft.module.scss';
 export const ResumeLeft = defineComponent({
@@ -7,48 +8,63 @@ export const ResumeLeft = defineComponent({
       type: String as PropType<string>
     }
   },
+  emits: ['update:show'],
   setup: (props, context) => {
-    return () => (
-      <div class={s.left}>
-        <div class={s.left_top}>
-          <div class={s.info_a}>
-            <img src="/src/assets/imgs/meself.jpg" alt="曹珂俭" />
-            <span class={s.info_name}>曹珂俭</span>
-            <div class={s.info_link}>
-              <div><svg class={s.svg}><use xlinkHref='#github_a'></use></svg>github</div>
-              <div><svg class={s.svg}><use xlinkHref='#juejin'></use></svg>掘金</div>
+    const showLeft = ref(true)
+    const onClick = () => {
+      showLeft.value = false
+      context.emit('update:show', true)
+    }
+    const onTrue = () => {
+      showLeft.value = true
+      context.emit('update:show', false)
+    }
+    return () => (<>
+      {showLeft.value ?
+        <div class={s.left}>
+          <div class={s.left_top}>
+            <div class={s.info_a}>
+              <img src="/src/assets/imgs/meself.jpg" alt="曹珂俭" />
+              <span class={s.info_name}>曹珂俭</span>
+              <div class={s.info_link}>
+                <div><svg class={s.svg}><use xlinkHref='#github_a'></use></svg>github</div>
+                <div><svg class={s.svg}><use xlinkHref='#juejin'></use></svg>掘金</div>
+              </div>
             </div>
           </div>
+          <div class={s.left_bottom}>
+            <div class={s.normalInfo}>
+              <span class={[s.normal]}>基础信息</span>
+              <div class={s.normal_info}>学历：本科（应届）</div>
+              <div class={s.normal_info}>电话：18339296532</div>
+              <div class={s.normal_info}>邮箱：1849201815@qq.com</div>
+              <div class={s.normal_info}>专业：数据科学与大数据技术</div>
+            </div>
+            <hr />
+            <div class={s.normalInfo}>
+              <span class={[s.normal]}>专业技能</span>
+              <Bar name={"Html"} process={'75%'}></Bar>
+              <Bar name={"CSS3"} process={'65%'}></Bar>
+              <Bar name={"Vue2"} process={'65%'}></Bar>
+              <Bar name={"JavaScript"} process={'55%'}></Bar>
+              <Bar name={"TypeScript"} process={'25%'}></Bar>
+              <Bar name={"Node.js"} process={'25%'}></Bar>
+              <Bar name={"Express"} process={'25%'}></Bar>
+              <Bar name={"工程化"} process={'25%'}></Bar>
+            </div>
+            <hr />
+            <div class={s.normalInfo}>
+              <span class={[s.normal]}>学校经历</span>
+              <div class={s.normal_info}>英语4级</div>
+              <div class={s.normal_info}>学校-青基智能科技工作室</div>
+              <div class={s.normal_info}>全国大学生计算机设计大赛</div>
+            </div>
+            <div><Button onClick={onClick}>关闭</Button></div>
+          </div>
         </div>
-        <div class={s.left_bottom}>
-          <div class={s.normalInfo}>
-            <span class={[s.normal]}>基础信息</span>
-            <div class={s.normal_info}>学历：本科（应届）</div>
-            <div class={s.normal_info}>电话：18339296532</div>
-            <div class={s.normal_info}>邮箱：1849201815@qq.com</div>
-            <div class={s.normal_info}>专业：数据科学与大数据技术</div>
-          </div>
-          <hr />
-          <div class={s.normalInfo}>
-            <span class={[s.normal]}>专业技能</span>
-            <Bar name={"Html"} process={'75%'}></Bar>
-            <Bar name={"CSS3"} process={'65%'}></Bar>
-            <Bar name={"Vue2"} process={'65%'}></Bar>
-            <Bar name={"JavaScript"} process={'55%'}></Bar>
-            <Bar name={"TypeScript"} process={'25%'}></Bar>
-            <Bar name={"Node.js"} process={'25%'}></Bar>
-            <Bar name={"Express"} process={'25%'}></Bar>
-            <Bar name={"工程化"} process={'25%'}></Bar>
-          </div>
-          <hr />
-          <div class={s.normalInfo}>
-            <span class={[s.normal]}>学校经历</span>
-            <div class={s.normal_info}>英语4级</div>
-            <div class={s.normal_info}>学校-青基智能科技工作室</div>
-            <div class={s.normal_info}>全国大学生计算机设计大赛</div>
-          </div>
-        </div>
-      </div>
+        : <Button class={s.button} onClick={onTrue}>展开</Button>
+      }
+    </>
     )
   }
 })
--- src/share/ResumePage.tsx
+++ src/share/ResumePage.tsx
@@ -1,12 +1,18 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref, watch } from 'vue';
 import { ResumeLeft } from '../components/Resume/ResumeLeft';
 import s from './ResumePage.module.scss';
 export const ResumePage = defineComponent({
   setup: (props, context) => {
+    const isChange = ref(false)
     return () => (
       <div class={s.wrapper}>
-        <ResumeLeft></ResumeLeft>
-        <div class={s.right}>2</div>
+        <ResumeLeft v-model:show={isChange.value}></ResumeLeft>
+        {isChange.value ?
+          <div>1</div> :
+          <div class={s.right}>
+            <div class={s.resume}></div>
+          </div>
+        }
       </div >
     )
   }
