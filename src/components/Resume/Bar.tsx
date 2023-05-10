302b28779a084c725350146089f91e94f16b56d0
曹珂俭 <1849201815@qq.com>
2023-05-10 19:47:36 +0800

    Bar组件新增BigBar

--- src/components/Resume/Bar.tsx
+++ src/components/Resume/Bar.tsx
@@ -16,8 +16,24 @@ export const Bar = defineComponent({
       <div class={s.normal_info}>
         <span>{props.name}</span>
         <div class={s.bar}>
-          <div class={s.process} style={{width:`${props.process}`}}></div>
+          <div class={s.process} style={{ width: `${props.process}` }}></div>
         </div>
+      </div>
+    )
+  }
+})
+
+export const BigBar = defineComponent({
+  props: {
+    process: {
+      type: String as PropType<string>,
+      default: "50%"
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <div class={s.process}>
+        <div class={s.bar} style={{ width: `${props.process}` }}>{props.process}</div>
       </div>
     )
   }
